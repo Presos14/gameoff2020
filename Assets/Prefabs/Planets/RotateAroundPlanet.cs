1179394cf0afece6098ebca169391692641db93b
SGG300 <sergio.g.g300@gmail.com>
2020-11-17 23:30:05 +0100

    RotateAroundPlanet logic

---
+++ Assets/Prefabs/Planets/RotateAroundPlanet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPlanet : MonoBehaviour
+{
+    public GameObject planet;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(planet.transform.position, Vector3.forward, speed * Time.deltaTime);
+    }
+}
