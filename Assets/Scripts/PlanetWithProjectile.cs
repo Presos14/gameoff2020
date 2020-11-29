1833ed8c9b92973441f2fa8a53a444e3523af063
SGG300 <sergio.g.g300@gmail.com>
2020-11-29 17:17:39 +0100

    PoolContainer for meteorites

--- Assets/Scripts/PlanetWithProjectile.cs
+++ Assets/Scripts/PlanetWithProjectile.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlanetWithProjectile : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     CircleCollider2D circleCollider2D;
     public GameObject meteoritePrefab;
+    public GameObject meteoritePoolParent;
     public float meteoriteRate = 0.3f;
     public float minLaunchForce = 100;
     public float maxLaunchForce = 400;
@@ -27,11 +29,26 @@ public class PlanetWithProjectile : MonoBehaviour
     void launchMeteorite() {
         Vector2 direction = Random.insideUnitCircle.normalized;
 
-        GameObject projectileObject = Instantiate(meteoritePrefab, rigidbody2d.position + direction * circleCollider2D.radius, Quaternion.identity);
+        GameObject projectileObject = InstanceMeteorites(direction);
 
-        projectileObject.transform.localScale *= Random.Range(1f, 2f); 
+        projectileObject.transform.localScale = Vector3.one*Random.Range(1f, 2f); 
 
         Meteorite meteorite = projectileObject.GetComponent<Meteorite>();
         meteorite.Launch(direction, Random.Range(minLaunchForce, maxLaunchForce));
     }
+
+    public GameObject InstanceMeteorites(Vector2 direction)
+    {
+        Meteorite meteorite = meteoritePoolParent.GetComponentsInChildren<Meteorite>(true).FirstOrDefault(m => m.gameObject.activeSelf == false);
+
+        if(meteorite == null)
+            return Instantiate(meteoritePrefab, rigidbody2d.position + direction * circleCollider2D.radius, Quaternion.identity, meteoritePoolParent.transform);
+        else
+        {
+            meteorite.gameObject.transform.position = rigidbody2d.position + direction* circleCollider2D.radius;
+            meteorite.gameObject.SetActive(true);
+            return meteorite.gameObject;
+        }
+    }
+
 }
